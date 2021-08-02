35bf2c21f1ae753fa42f57715262fd488b627651
Edgar Olivar <edgarolivar16@gmail.com>
2021-08-02 16:03:43 -0400

    ⚙ navigation config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import StackNavigation from './src/navigation/StackNavigation';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigation />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>HOME SCREEN!!</Text>
+      <Ionicons name="airplane-outline" size={24} color="black" />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
